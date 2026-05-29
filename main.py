04d081d91dfc8a2f380ddba8540c1da1f930be85
aryansharma-cmd <aryansharma@traffury.in>
2026-05-30 01:27:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -461,7 +461,7 @@ def auto_run(data: AutoRequest):
     }
 
     easycron_headers = {
-        "X-API-Key": "ac9f7b2335ad6f6248e332cf42252ee5",
+        "X-API-Key": "44ec89d1eb10cc0022c117eaf2c4f63e",
         "Content-Type": "application/json"
     }
 
@@ -480,7 +480,7 @@ def auto_run(data: AutoRequest):
 @app.get("/cron-jobs")
 def list_cron_jobs():
     easycron_headers = {
-        "X-API-Key": "ac9f7b2335ad6f6248e332cf42252ee5",
+        "X-API-Key": "44ec89d1eb10cc0022c117eaf2c4f63e",
         "Content-Type": "application/json"
     }
     response = requests.get(
@@ -494,7 +494,7 @@ def list_cron_jobs():
 @app.delete("/cron-jobs/{cron_job_id}")
 def delete_cron_job(cron_job_id: int):
     easycron_headers = {
-        "X-API-Key": "ac9f7b2335ad6f6248e332cf42252ee5",
+        "X-API-Key": "44ec89d1eb10cc0022c117eaf2c4f63e",
         "Content-Type": "application/json"
     }
     response = requests.delete(
